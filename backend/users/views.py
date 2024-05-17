e80f8a26f5c5509fd29dff97f79afe51d73f0e50
Danial <danial_ikar@mail.ru>
2024-05-17 14:43:07 +0500

    Changed login requirements and added admin panel

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserSerializer
+from .serializers import UserSignUpSerializer
+
 
 class UserCreate(generics.CreateAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserSignUpSerializer
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
