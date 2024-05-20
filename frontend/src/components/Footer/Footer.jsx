069441a2b103f2dc74110b823e682c83bae7be63
Meirambekov Nursultan <meirambekov.n2005@gmail.com>
2024-05-20 12:29:59 +0500

    update

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
     return(
         <>
@@ -5,27 +7,13 @@ export default function Footer() {
 
 <footer>
     <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8 mx-auto max-w-screen-xl" />
-    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl font-['Montserrat'] mx-auto p-4 md:py-8">   
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl font-['Montserrat'] mt-[-40px] md:py-8">   
         <div class="sm:flex sm:items-center sm:justify-between w-[570px]">
-            <a href="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            <Link to="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <span class="self-center text-xl font-medium whitespace-nowrap ">HackerPlace</span>
-            </a>
-            <ul class="flex flex-row justify-around w-96 items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
-            </ul>
+            </Link>
         </div>
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline mr-4">Flowbite™</a> All Rights Reserved.</span>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" class="hover:underline mr-4">HackerPlace</Link> All Rights Reserved.</span>
     </div>
 </footer>
 
--- frontend/src/components/Play/Play.jsx
+++ frontend/src/components/Play/Play.jsx
@@ -1,10 +1,125 @@
 
-
+import Navbar from "../Navbar/Navbar"
+import Footer from "../Footer/Footer"
 
 export default function Play() {
+    const exercises = [
+        {
+           id: 1,
+           title: "Advanced Web Exploitation",
+           date: "01",
+           task: "Discover sophisticated methods to exploit and secure web applications effectively",
+           category: "webExp",
+           exam: "IELTS"
+        },
+        {
+           id: 2,
+           title: "Steganography Techniques",
+           date: "02",
+           task: "Learn advanced methods for hiding and detecting information in media",
+           category: "steganography",
+           exam: "IELTS"
+        },
+        {
+           id: 3,
+           title: "Expert Open-Source Intelligence (OSINT)",
+           date: "03",
+           task: "Master advanced techniques for gathering and analyzing publicly available data",
+           category: "osint",
+           exam: "IELTS"
+        },
+        {
+           id: 4,
+           title: "Intermediate Cryptography",
+           date: "04",
+           task: "Enhance your understanding of cryptographic algorithms and secure communication methods",
+           category: "cryptography",
+           exam: "IELTS"
+        },
+        {
+           id: 5,
+           title: "Introductory Reverse Engineering",
+           date: "05",
+           task: "Introduction to software disassembly, analysis, and vulnerability detection techniques",
+           category: "reverseEng",
+           exam: "IELTS"
+        },
+        {
+           id: 6,
+           title: "Binary Exploitation",
+           date: "06",
+           task: "Basic concepts of binary exploitation, including buffer overflows and ROP",
+           category: "binaryExp",
+           exam: "IELTS"
+        }
+     ]
+
+    //  const navigate = useNavigate();
+//    const handleButtonClick = (exercise) => {
+//       if (
+//          exercise.category == "webExp"
+//       ) {
+//          navigate("/webExp");
+//       } else if (
+//          exercise.category == "steganography"
+//       ) {
+//          navigate("/steganography");
+//       } else if (
+//          exercise.category == "taskWriting1" ||
+//          exercise.category == "taskWriting2"
+//       ) {
+//          navigate(`/practice/writing?category=${exercise.category}`);
+//       } else if (exercise.category == "readingTasks") {
+//          navigate(`/practice/readingMock?category=${exercise.category}`);
+//       } else if (exercise.category == "listeningTasks") {
+//          navigate(`/practice/listeningMock?category=${exercise.category}`);
+//       } else if (exercise.category == "speakingTasks") {
+//          navigate(`/practice/speakingMock?category=${exercise.category}`);
+//       }
+//       {
+//          console.log("hello");
+//       }
+//       window.scrollTo(0, 0);
+//    };
     return(
         <>
-            play
+            <Navbar />
+                <div className="mt-20 font-['Montserrat']">
+                <div className="text-center mt-16">
+                    <p className="text-black-700 text-4xl font-bold">
+                        Categories
+                    </p>
+                    <p className="text-violet-600 text-3xl mt-4">Tasks for each category</p>
+                </div>
+                {exercises.map((exercise) => (
+               <div
+                  key={exercise.id}
+                  className="hoverShadow flex flex-wrap justify-between items-center mx-auto max-w-screen-xl font-['Montserrat'] h-32 border-[1px] rounded-md mb-6 mt-8 border-practiceBorderGray p-6 ss:p-8"
+               >
+                  <div className="flex flex-row gap-2 xs:gap-4 ss:gap-6">
+                     <p className="text-violet-600 font-black text-5xl">
+                        {exercise.date}
+                     </p>
+
+                     <div className="ml-4">
+                        <p className="text-base ss:text-xl text-smrtBlack">
+                           {exercise.title}
+                        </p>
+                        <p className="font-medium text-practiceGray">
+                           {exercise.task}
+                        </p>
+                     </div>
+                  </div>
+                  <button
+                     className="hoverButton w-36 h-11 border-[1px] border-smrtBlue text-smrtBlue text-sm rounded-md"
+                     onClick={() => handleButtonClick(exercise)}
+                  >
+                     Start
+                  </button>
+               </div>
+            ))}
+                </div>
+            <Footer />
         </>
     )
 }
