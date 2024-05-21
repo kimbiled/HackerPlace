6c5723447c0f82c3a95c09fd8f7e207212db508d
Danial <danial_ikar@mail.ru>
2024-05-21 17:42:27 +0500

    Fixed submission

--- backend/assignments/serializers.py
+++ backend/assignments/serializers.py
@@ -1,24 +1,37 @@
 from rest_framework import serializers
 from .models import Module, Assignment, Hint, UserAssignment
+from datetime import timedelta
 
 class HintSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hint
-        fields = ['id', 'text', 'image']
+        fields = ["id", "text", "image"]
+
 
 class AssignmentSerializer(serializers.ModelSerializer):
     hints = HintSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Assignment
-        fields = ['id', 'title', 'description', 'image', 'video', 'file', 'answer', 'hints']
+        fields = [
+            "id",
+            "title",
+            "description",
+            "image",
+            "video",
+            "file",
+            "answer",
+            "hints",
+        ]
+
 
 class ModuleSerializer(serializers.ModelSerializer):
     assignments = AssignmentSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Module
-        fields = ['id', 'title', 'description', 'assignments']
+        fields = ["id", "title", "description", "assignments"]
+
 
 class UserAssignmentSerializer(serializers.ModelSerializer):
     assignment = AssignmentSerializer(read_only=True)
@@ -26,16 +39,19 @@ class UserAssignmentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserAssignment
-        fields = ['id', 'assignment', 'completed', 'completed_at', 'time_taken']
-        read_only_fields = ['completed', 'completed_at']
+        fields = ["id", "assignment", "completed", "completed_at", "time_taken"]
+        read_only_fields = ["completed", "completed_at"]
+
 
 class UserAssignmentSubmitSerializer(serializers.ModelSerializer):
     time_taken = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = UserAssignment
-        fields = ['time_taken']
+        fields = ["time_taken"]
 
     def update(self, instance, validated_data):
-        instance.complete(time_taken=validated_data.get('time_taken'))
+        instance.complete(
+            time_taken=timedelta(seconds=validated_data.get("time_taken"))
+        )
         return instance
